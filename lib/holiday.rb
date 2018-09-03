23c9546fa443a81976a5ed887cef4a1d1f4b63d5
zhifeng ou <zhifengou@hotmail.com>
2018-09-03 20:11:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -169,7 +169,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.each do |key,value|
 value.each do |key,value|
   if value.include?("BBQ")
-    key_arr=key_arr.pukey
+    key_arr=key_arr<<this.key
   end
 end
 return key_arr
